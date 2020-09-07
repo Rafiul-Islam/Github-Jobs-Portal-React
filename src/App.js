8c9acc9e24319356bf9493d2e3868a02cded10fe
Rafiul Isalam <[shohel.rana@mymedicalhub.com]>
2020-09-08 01:44:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UseFatchJobs from "./UseFatchJobs";
+
+function App() {
+    return (
+        <div>
+            <UseFatchJobs/>
+        </div>
+    );
+}
+
+export default App;
