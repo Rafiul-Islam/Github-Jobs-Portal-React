f233e3f54cc45319a89f54b0efc5e32eeb87fcde
Rafiul Isalam <[shohel.rana@mymedicalhub.com]>
2020-09-08 11:59:22 +0600

    Fresh Project

--- src/UseFatchJobs.js
+++ src/UseFatchJobs.js
@@ -1,23 +1,6 @@
 import React, {Component} from 'react';
-import axios from "axios";
 
 class UseFatchJobs extends Component {
-    state = {
-        persons: []
-    }
-
-    componentDidMount() {
-        axios.get('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json')
-            .then(res => {
-                const persons = res.data;
-                this.setState({persons});
-                console.log(this.state.persons[1])
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error.message);
-            })
-    }
 
     render() {
         return (
